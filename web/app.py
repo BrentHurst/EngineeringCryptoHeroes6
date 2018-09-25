42a97b376850121ea6f26e8f7af076ea0b60aa84
Chris Dean <chrisdean258@gmail.com>
2018-09-25 13:32:32 -0400

    dockerized

--- web/app.py
+++ web/app.py
@@ -1,9 +1,10 @@
-from flask import Flask 
+#!/usr/bin/env python3
+from flask import Flask
 
-app = Flask(__name__) 
+app = Flask(__name__)
 
-@app.route('/') 
-def index(): 
-	return "Hello World!" 
+@app.route('/')
+def index():
+	return "Hello World!"
 
 
