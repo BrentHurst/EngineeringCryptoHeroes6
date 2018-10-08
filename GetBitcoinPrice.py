bdb96abe55872d47b49df231699c5f1edde3f7e8
Brent Hurst <seqvalley77@aol.com>
2018-10-07 22:21:39 -0400

    Added GetBitcoinPrice.py, which as of now is a module with a function that will just return a float representing the price of Bitcoin in USD

---
+++ GetBitcoinPrice.py
@@ -0,0 +1,9 @@
+import requests
+
+BitcoinPriceUrl = 'http://api.coindesk.com/v1/bpi/currentprice.json'
+
+def BitcoinPrice():
+    return requests.get(BitcoinPriceUrl).json()['bpi']['USD']['rate_float']
+
+if __name__ == "__main__":
+    print(BitcoinPrice())
