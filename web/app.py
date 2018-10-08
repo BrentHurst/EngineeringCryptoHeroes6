d2c8a909e9725ad505b082b12764ab0f8656465d
Chris Dean <chrisdean258@gmail.com>
2018-10-08 13:30:40 -0400

    static files

--- web/app.py
+++ web/app.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def index():
 	return "Hello World!"
-
 
