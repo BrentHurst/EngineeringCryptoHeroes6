c3278ea15101936e4044df6d1046a884650a5784
Brent Hurst <seqvalley77@aol.com>
2018-11-02 16:38:16 -0400

    CurrentPrice.py is done

--- modules/marketstate/historicaldata.py
+++ modules/marketstate/historicaldata.py
@@ -3,6 +3,7 @@
 # functions for historical crypto data
 
 import requests
+from os import system
 
 url_array = []
 url_array.append('https://apiv2.bitcoinaverage.com/indices/global/history/')
@@ -11,10 +12,10 @@ url_array.append('&format=')
 
 # Don't use these helper functions
 def ____MakeURLString(s):
-    return url_array[0] + s + url_array[1] + 'alltime' + url_array[2] + 'json'
+    return url_array[0] + s + url_array[1] + 'alltime' + url_array[2] + 'csv'
 
 def ____GetTheJson(s):
-    return requests.get(____MakeURLString(s)).json()
+    return requests.get(____MakeURLString(s)).text
 # Don't use the above helper functions
 
 
@@ -30,3 +31,6 @@ def HistoricalEtheriumPrice():
 
 def HistoricalLitecoinPrice():
     return ____GetTheJson('LTCUSD')
+
+if __name__ == "__main__":
+    print("Why did you run this file?")
