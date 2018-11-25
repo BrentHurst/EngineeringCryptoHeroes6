14d89d9b3f497b8ef22c5208ce9ea2fbf3fba6be
Brent Hurst <seqvalley77@aol.com>
2018-11-25 17:26:13 -0500

    makeTrades

--- modules/db/database.py
+++ modules/db/database.py
@@ -31,3 +31,36 @@ class coindb():
     def updateMarketState(self,coinID,exchangeID,value):
         c = self.connection.cursor()
         c.execute("insert into cryptoData values (?, ?, ?, CURRENT_TIMESTAMP)",(coinID,exchangeID,value))
+
+    def getIncompleteTrades(self):
+        c = self.connection.cursor()
+        return c.execute("select rowid, * from trades where completed = 0").fetchall()
+
+    def getCurrentValue(self,coin,exchange):
+        c = self.connection.cursor()
+        return c.execute("select value from cryptoData where coinID=? and exchangeID=? order by timestamp desc limit 1",(coin,exchange)).fetchall()[0][0]
+
+    def markTradeComplete(self,tradeID):
+        c = self.connection.cursor()
+        c.execute("update trades set completed = 1 where rowid = ?",tradeID)
+
+    def updateCoinAmount(self,tradePoolID,amount,coinFrom,coinTo,coinFromVal,coinToVal):
+        c = self.connection.cursor()
+        c.execute("update tradingPoolCoinData set coinAmount = (coinAmount - (? / ?)) where tradingPoolID = ? and coinID = ?",(amount,coinFromVal,tradePoolID,coinFrom))
+        c.execute("update tradingPoolCoinData set coinAmount = (coinAmount + (? / ?)) where tradingPoolID = ? and coinID = ?",(amount,coinToVal,tradePoolID,coinTo))
+
+        exchanges = ['gdax','bitstamp','binance']
+        d = {'BTC':-1,'LTC':-1,'ETH':-1}
+        for key in d:
+            for ex in exchanges:
+                val = self.getCurrentValue(key,ex)
+                if val > d[key]:
+                    d[key] = val
+
+        rows = c.execute("select coinID, coinAmount from tradingPoolCoinData where tradingPoolID = ?",tradePoolID).fetchall()
+
+        s = 0
+        for coin,coinamt in rows:
+            s += d[coin] * coinamt
+
+        c.execute("update tradingPools set value = ? where rowid = ?",(s,tradePoolID))
--- modules/trades/makeTrades.py
+++ modules/trades/makeTrades.py
@@ -3,16 +3,24 @@
 from db.database import *
 
 
+
+
+
+             # test getIncompleteTrades()
+             # test markTradeComplete()
+
+             # amount is in usd
+
 def maketrades():
     with coindb() as db:
         todo = db.getIncompleteTrades()
         for tradeID, tradePoolID, coinFrom, coinTo, amount, exchangeID, completed in todo:
-             coinFromVal = db.getCurrentValue(coinFrom, exchangeID)
-             coinToVal = db.getCurrentValue(coinTo, exchangeID)
+            coinFromVal = db.getCurrentValue(coinFrom, exchangeID)
+            coinToVal = db.getCurrentValue(coinTo, exchangeID)
 
-             #update the pool coin data
+            # TradingPoolCoinData.coinAmount -= amount / coinFromVal
+            # TradingPoolCoinData.coinAmount += amount / coinToVal
+            # TradingPools.value = sum(all coins on their max exchange)
+            db.updateCoinAmount(tradePoolID,amount,coinFrom,coinTo,coinFromVal,coinToVal)
 
-             db.markTradeComplete(tradeID)
-
-
-
+            db.markTradeComplete(tradeID)
